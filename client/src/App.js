c8d3cc7d2519915fa7893fb5561a64b457cc554b
Hassanabbaass <hassan.abbaass@gmail.com>
2023-01-30 12:50:09 +0200

    React Router Dom V6 implementation

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,48 @@
 import NavbarOne from './components/navbar/NavbarOne';
 import './App.css'
 import LoginRegister from './components/loginRegister/LoginRegister';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import UserPage from './pages/userPages/UserPage';
+import AdminPage from './pages/adminPages/AdminPage';
+
+const router = createBrowserRouter([
+  {
+    element: (
+      <NavbarOne/>
+    ),
+    children: [
+      {
+        path: "",
+        element: (
+          <LoginRegister/>
+        ),
+      },
+      {
+        path: "/user",
+        element: (
+          <UserPage/>
+        ),
+      },
+      {
+        path: "/admin",
+        element: (
+          <AdminPage/>
+        ),
+      },
+    ]
+  }
+
+]);
 
 function App() {
   return (
     <div className="App">
-      <NavbarOne/>
 
       <div className='AppContent'>
-        <LoginRegister/>
+        <RouterProvider router={router}/>
       </div>
       
     </div>
--- client/src/components/navbar/NavbarOne.jsx
+++ client/src/components/navbar/NavbarOne.jsx
@@ -4,25 +4,30 @@ import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
 
 import SystemImage from '../../assets/SystemImage.png'
+import { Outlet } from 'react-router-dom';
 
 const NavbarOne = () => {
   return (
     <div>
         <Navbar bg="dark" variant="dark">
           <Container>
-            <Navbar.Brand href="#home">
+            
+            <Navbar.Brand>
               <img
                 alt=""
                 src={SystemImage}
                 width="30"
                 height="30"
                 className="d-inline-block align-top"
-              />{' '}
+              />
+              {' '}
               LOGIN SYSTEM
             </Navbar.Brand>
+            
             <Button variant="outline-light">Login</Button>
           </Container>
       </Navbar>
+      <Outlet/>
     </div>
   )
 }
